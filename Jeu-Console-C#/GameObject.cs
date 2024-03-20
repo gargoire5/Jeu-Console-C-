c9eb2da5e43dd7a1247eefe6b969c362d02c6921
gargoire5 <gportevin@gaming.tech>
2024-03-20 15:03:27 +0100

    debut GameObject

--- Jeu-Console-C#/GameObject.cs
+++ Jeu-Console-C#/GameObject.cs
@@ -8,6 +8,30 @@ namespace Jeu_Console_C_
 {
     internal class GameObject
     {
+        public string Name { get; set; }
+        public int Health { get; set; }
+        public int AttackDamage { get; set; }
 
+        public GameObject (string name, int health, int attackDamage)
+        {
+            Name = name;
+            Health = health;
+            AttackDamage = attackDamage;
+        }
+
+        public void TakeDamage(int damage)
+        {
+            Health -= damage;
+            if (Health < 0)
+            {
+                Health = 0;
+            }
+        }
+
+        public void Attack(GameObject target) 
+        {
+            target.TakeDamage(AttackDamage);
+            Console.WriteLine($"{Name} attaque {target.Name} et inflige {AttackDamage} dégât");
+        }
     }
 }

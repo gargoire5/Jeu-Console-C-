57fc12a24479f499598ac897a3e602ac0edd083f
ManaIron <117281679+ManaIron@users.noreply.github.com>
2024-03-25 02:21:12 +0100

    Update Player.cs

--- Jeu-Console-C#/Player.cs
+++ Jeu-Console-C#/Player.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,5 +7,27 @@ namespace Jeu_Console_C_
 {
     public class Player
     {
+        public string Name { get; set; }
+        public List<Techmons> TechmonsCaptures { get; set; }
+
+        public Player(string name)
+        {
+            Name = name;
+            TechmonsCaptures = new List<Techmons>();
+        }
+        public void CapturerTechmon(Techmons techmon)
+        {
+            TechmonsCaptures.Add(techmon);
+            Console.WriteLine($"{Name} a capturé {techmon.Name}!");
+        }
+
+        /* public void CapturerTechmons(Techmons Techmons)
+         {
+             TechmonsCaptures.Add(Techmons);
+             Console.WriteLine($"{Name} a capturé {Techmons.Name}!");
+         }*/
     }
 }
+
+
+

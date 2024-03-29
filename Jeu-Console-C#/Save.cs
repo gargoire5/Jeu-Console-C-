a316b38362d3c855d3f2d472ab0823d1d026802a
ManaIron <117281679+ManaIron@users.noreply.github.com>
2024-03-29 05:54:48 +0100

    Create Save.cs

---
+++ Jeu-Console-C#/Save.cs
@@ -0,0 +1,19 @@
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.IO;
+
+public class Save
+{
+    public static void SaveToFile<T>(string filePath, T data)
+    {
+        string json = JsonConvert.SerializeObject(data, Formatting.Indented);
+        File.WriteAllText(filePath, json);
+    }
+
+    public static T LoadFromFile<T>(string filePath)
+    {
+        if (!File.Exists(filePath)) return default(T);
+        string json = File.ReadAllText(filePath);
+        return JsonConvert.DeserializeObject<T>(json);
+    }
+}
